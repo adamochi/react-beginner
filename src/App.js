6fc9ca1bdc7f40af86997df2ed3a3e475000af76
adamochi <honeyrumballs@gmail.com>
2022-05-31 17:40:55 +1000

    7.0 todo list part one

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
-import Button from "./Button";
+import { useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo(""); // we never modify directly with toDos.push X
+  };
+  console.log(toDos);
+  return (
+    <form onSubmit={onSubmit}>
+      <h1 className={styles.title}>My To Dos ({toDos.length})</h1>
+      <input
+        onChange={onChange}
+        value={toDo}
+        type="text"
+        placeholder="Write your to do..."
+      />
+      <button>submit</button>
+    </form>
+  );
+}
+
+export default App;
+/*
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once");
+  }, []); // [] where dependencies go
+  useEffect(() => {
+    if (keyword.length > 3) {
+      console.log("I run when 'keyword' changes.");
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1 className={styles.title}>Welcome Back to React!</h1>
+      <h2>{counter}</h2>
+      <button onClick={onClick}>click me</button>
       <Button text={"continue"} />
     </div>
   );
 }
 
-export default App;
+*/
